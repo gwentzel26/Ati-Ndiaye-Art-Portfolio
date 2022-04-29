c8c99c11013a7961ae37d557ae1191ee79d4ec92
Gavin Wentzel <gavinwentzel@gmail.com>
2022-04-29 13:26:52 -0600

    Completed frontend file structure

---
+++ client/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import {card} from "react-bootstrap";
+import '../css/Home.css'
+import { LinkContainer } from "react-router-bootstrap";
+
+const Home = () => {
+
+
+    return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
+
+}
+
+
+export default Home;
