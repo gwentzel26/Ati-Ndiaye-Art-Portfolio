25aedd0c1dcef9af1a4576a27609d7195246a282
Gavin Wentzel <gavinwentzel@gmail.com>
2023-12-01 18:00:23 -0500

    update repo

--- client/art-portfolio/src/components/Navbar.jsx
+++ client/art-portfolio/src/components/Navbar.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
+import {Link} from "react-scroll"
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
+    const [nav, setNav ] = useState(false);
+    const Links = [
+        {
+            id: 1,
+            child: (
+                <>
+                    E-mail <HiOutlineMail size={30} />
+                </>
+            ),
+            href: "mailto:atallah.ndiaye@gmail.com"
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                 Resume <BsFillPersonLinesFill size={30} />
+                </>
+            )
+        }
+    ]
+
   return (
     <div className="flex items-center w-full h-20 text-white bg-[#d5968c] px-4 fixed">
-        <div>
-            <h1 className='text-5xl text-white ml-5 font-signature'>ATI NDIAYE</h1>
-        </div>
+        <Link to={"Home"} smooth duration={500}> <h1 className="text-5xl font-signature ml-2"> Gavin </h1>
+        </Link>
 
-        {/* social links */}
-        <div>
-
-        </div>
-    
+        {/* <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 md:hidden snap-center">
+        
+        {nav ? <FaTimes size={35} /> : <FaBars size={35} /> }
+        </div> */}
+        {/* social links desktop */}
+        {/* {nav && (
+            <ul className=''>
+                 {Links.map(({id, child, href}) => ( <li key={id} className={"flex items-start w-full"}>
+              <a
+                  href={href}
+                  className=""
+                  target='_blank'
+                  rel="noreferrer"
+                  >
+                      {child}
+                  </a>
+                  </li>))}
+                  </ul>     
+        )} */}
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
